57c4172aa1d146ee434048f6369d7b66be530e69
Jorge Gustavo Rocha <jgr@geomaster.pt>
2016-08-21 18:45:57 +0200

    MapQuest layer was discarded

--- classic/src/view/geo/MapCanvas.js
+++ classic/src/view/geo/MapCanvas.js
@@ -1,5 +1,4 @@
-
-Ext.define("Admin.view.geo.MapCanvas",{
+Ext.define("Admin.view.geo.MapCanvas", {
     //extend: "Ext.panel.Panel",
     extend: "GeoExt.component.Map",
     alias: 'widget.geo-mapcanvas',
@@ -22,24 +21,14 @@ Ext.define("Admin.view.geo.MapCanvas",{
     map: new ol.Map({
         layers: [
             new ol.layer.Tile({
-                legendUrl: 'http://wiki.openstreetmap.org/w/images/thumb/7/79/Public-images-osm_logo.svg/256px-Public-images-osm_logo.svg.png',
-                name: 'Stamen watercolor',
-                source: new ol.source.Stamen({
-                    layer: 'watercolor'
-                })
-            }),
-            new ol.layer.Tile({
-                legendUrl: 'https://otile4-s.mqcdn.com/tiles/1.0.0/sat/4/4/7.jpg',
-                source: new ol.source.MapQuest({layer: 'sat'}),
-                name: 'MapQuest Satellite',
-                visible: false
+                source: new ol.source.OSM()
             })
         ],
         view: new ol.View({
-            center: ol.proj.fromLonLat( [ 7, 51 ] ),
+            center: ol.proj.fromLonLat([7, 51]),
             zoom: 5
         }),
-        interactions : ol.interaction.defaults({doubleClickZoom :false})
+        interactions: ol.interaction.defaults({doubleClickZoom: false})
     })
 
 });
